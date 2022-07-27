5cb4935eadba96ec7fee708740de3ce47864431f
danypr008 <65912927+danypr008@users.noreply.github.com>
2022-07-27 15:03:53 -0500

    pause, freeplay, dialogue

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -12,6 +12,7 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
+import flixel.FlxCamera;
 
 class PauseSubState extends MusicBeatSubstate
 {
@@ -73,6 +74,15 @@ class PauseSubState extends MusicBeatSubstate
 		}
 
 		changeSelection();
+
+		#if mobileC
+		addVirtualPad(UP_DOWN, A_B);
+		
+		var camcontrol = new FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+		#end
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 	}
